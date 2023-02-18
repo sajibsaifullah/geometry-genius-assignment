9307ef3aee092c6d72f6d973237df8be24935ce5
sajibsaifullah <dusajibphy@gmail.com>
2023-02-18 11:08:04 +0600

    parallelogram added

--- js/geometry.js
+++ js/geometry.js
@@ -47,6 +47,46 @@ document.getElementById('btn-rectangle').addEventListener('click', function () {
     setInCalculationById(serial, figureName, area);
 })
 
+// Parallelogram
+/* document.getElementById('btn-parallelogram').addEventListener('click', function () {
+    serial += 1;
+    const figureName = document.getElementById('fig-parallelogram').innerText;
+    const firstData = document.getElementById('first-parallelogram-data').innerText;
+    const secondData = document.getElementById('second-parallelogram-input').innerText;
+
+    const firstDataValue = parseFloat(firstData);
+    const secondDataValue = parseFloat(secondData);
+
+    const area = (firstDataValue * secondDataValue).toFixed(2);
+
+    setInCalculationById(serial, figureName, area);
+
+}) */
+// parallelogram
+document.getElementById('btn-parallelogram').addEventListener('click', function () {
+    serial += 1;
+    const inputData = getDataById('fig-parallelogram', 'first-parallelogram-data', 'second-parallelogram-input');
+
+
+    const area = (parseFloat(inputData.firstData) * parseFloat(inputData.secondData)).toFixed(2);
+
+    setInCalculationById(serial, inputData.figureName, area);
+
+})
+// common get data function
+function getDataById(figName, dataOne, dataTwo) {
+    const figureName = document.getElementById(figName).innerText;
+    const firstData = document.getElementById(dataOne).innerText;
+    const secondData = document.getElementById(dataTwo).innerText;
+
+    const inputData = {
+        figureName: figureName,
+        firstData: firstData,
+        secondData: secondData
+    }
+    return inputData;
+}
+
 // common set function
 function setInCalculationById(serialNo, figureName, area) {
     const parentContainer = document.getElementById('table-container');
